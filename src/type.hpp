dfe1a31130db7b0b49f38f8eaf075ede3506f9ca
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2026-05-14 19:28:48 -0300

    refactor: The types that don't know they're dead
    The auditors have discovered something unsettling in type.hpp. They're
    everywhere. Walking around like regular functions. They only see what
    they want to see. They don't know they're dead.
    
    The first is draw_on_screen. It haunts the GameObject base class with
    unfinished business, waiting for a meeting about a "FIXME" note left on
    its desk over twenty years ago. If ever invoked, it attempts to interact
    with the living world by secretly zeroing out the global scroll_x books,
    calling the draw team, and restoring the records. This was a desperate
    and hacky attempt to manipulate shared infrastructure. Luckily, it was
    never called. We held the meeting it was waiting for and now it can rest.
    
    The second is string_list_type. He was found hiding in plain sight right
    next to his modern replacement (StringList). He'd been standing guard
    like a forgotten soldier, still clutching a char** and two int`s,
    fighting a C-style war that ended decades ago. He didn't understand why
    no one had called him in years, or why the std::vector next door was
    doing all the work. We explained that it's over and helped him cross
    into the light.

--- src/type.hpp
+++ src/type.hpp
@@ -39,35 +39,10 @@ public:
   virtual void action(float frame_ratio) = 0;
   virtual void draw() = 0;
   virtual std::string type() = 0;
-  /* Draw ignoring the scroll_x value. FIXME: Hack? Should be discussed. @tobgle*/
-  void draw_on_screen(float x = -1, float y = -1)
-  {
-    base_type btmp = base;
-    if(x != -1 || y != -1)
-    {
-      btmp = base;
-      if(x != -1)
-        base.x = x;
-      if(y != -1)
-        base.y = y;
-    }
-    float tmp = scroll_x;
-    scroll_x = 0; draw();
-    scroll_x = tmp;
-    base = btmp;
-  };
-
   void move_to(float x, float y) { base.x = x; base.y = y; };
 
   base_type base;
   base_type old_base;
-};
-
-struct string_list_type
-{
-  int num_items;
-  int active_item;
-  char **item;
 };
 
 using StringList = std::vector<std::string>;
